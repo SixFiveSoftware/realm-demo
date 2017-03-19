235882f147a3b5a0a368fed22ea31545ec1aa431
BJ Miller <john.miller@dxydoes.com>
2017-03-18 21:35:25 -0400

    Add xcode project

---
+++ RealmTasks/RealmTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmTasks
+//
+//  Created by BJ Miller on 3/18/17.
+//  Copyright © 2017 BJ Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
